850fa1ad892f430c12340568c22c35847f360ceb
Bill Glesias <bglesias@gmail.com>
2022-10-20 13:14:03 -0400

    working reproduction

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
     experimentalSessionAndOrigin: true
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,22 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit('example.cypress.io')
-    cy.readFile("cypress/fixtures/example.csv").then((text) => {
-      cy.readFile("cypress/fixtures/example-copy.csv").should("eq", text);
-    });
+describe("empty spec", { pageLoadTimeout: 2000 }, () => {
+  it("no op failure (downloads the file)", () => {
+    cy.on("fail", () => false);
+    cy.visit("cypress/fixtures/index.html");
+
+    cy.get("a").click();
+  });
+
+  it("reads the file downloaded and makes sure they are equal", () => {
+    cy.visit("cypress/fixtures/index.html");
+    cy.readFile("cypress/downloads/Sample-Spreadsheet-10-rows.csv").then(
+      (text) => {
+        // return carriages exist in the downloaded version
+        text = text.replaceAll('\r', '')
+        // with the first test unskipped, this will crash the runner =(
+        cy.readFile(
+          "Sample-Spreadsheet-10-rows-compare.csv"
+        ).should("eq", text);
+      }
+    );
   });
 });
